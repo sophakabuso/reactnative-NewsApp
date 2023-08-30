f0c453602f6a2533986ef8d9ad448803d52fd9ae
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-08-30 14:18:25 +0200

    first commit

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import CategoriesScreen from '../screens/CategoriesScreen';
+import ArticleListScreen from '../screens/ArticleListScreen';
+import ArticleDetailScreen from '../screens/ArticleDetailScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Categories">
+        <Stack.Screen name="Categories" component={CategoriesScreen} />
+        <Stack.Screen name="ArticleList" component={ArticleListScreen} />
+        <Stack.Screen name="ArticleDetail" component={ArticleDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
